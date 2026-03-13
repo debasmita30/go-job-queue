a4b6d3d852aa8d8ca8c1a356962ac17a77a98dec
Debasmita Chatterjee <115386608+debasmita30@users.noreply.github.com>
2026-03-13 15:07:07 +0530

    Update router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -36,20 +36,3 @@ func Setup(db *sql.DB, dispatcher *queue.Dispatcher) *gin.Engine {
 
 	return r
 }
-```
-
----
-
-**`go.mod`**
-```
-module github.com/debasmita30/go-job-queue
-
-go 1.21
-
-require (
-	github.com/gin-contrib/cors v1.4.0
-	github.com/gin-gonic/gin v1.9.1
-	github.com/google/uuid v1.4.0
-	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
-)
