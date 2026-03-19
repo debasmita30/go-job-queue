b9e4967a2439ca493370ca4746b74bb4dda84cc0
Debasmita Chatterjee <115386608+debasmita30@users.noreply.github.com>
2026-03-19 11:47:10 +0530

    Update urls.py

--- django/config/urls.py
+++ django/config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import JobViewSet
+from api.views import JobViewSet, StatsViewSet
 
 router = DefaultRouter()
 router.register(r'jobs', JobViewSet, basename='job')
+router.register(r'stats', StatsViewSet, basename='stats')  # ← ADD THIS
 
 urlpatterns = [
     path('admin/', admin.site.urls),
