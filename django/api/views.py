78e6f172046e17a5a3d3775ff95d3b86b0b24850
Debasmita Chatterjee <115386608+debasmita30@users.noreply.github.com>
2026-03-23 13:46:21 +0530

    Update views.py

--- django/api/views.py
+++ django/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Job
 from .serializers import JobSerializer
- 
+
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
@@ -26,7 +26,7 @@ class JobViewSet(viewsets.ModelViewSet):
             },
             status=status.HTTP_201_CREATED
         )
- 
+
 class StatsViewSet(viewsets.ViewSet):
     
     def list(self, request):
